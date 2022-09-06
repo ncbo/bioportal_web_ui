00404650080a3e5e8bb1e1f65fe7cbea6ac3e0ea
Alex Skrenchuk <alexskr@stanford.edu>
2022-09-06 15:14:11 -0700

    unset tty

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,7 +20,7 @@ set :deploy_to, "/srv/rails/#{fetch(:application)}"
 # set :log_level, :debug
 
 # Default value for :pty is false
-set :pty, true
+# set :pty, true
 
 # Default value for :linked_files is []
 # set :linked_files, %w{config/bioportal_config.rb config/database.yml public/robots.txt}
