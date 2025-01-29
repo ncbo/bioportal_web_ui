b8ce017a28ddd277d52b665a54b8bc31c4af92bc
Jennifer Vendetti <vendetti@stanford.edu>
2025-01-29 13:15:53 -0800

    Restore theme-variables SCSS file
    Has to be handled by Sprockets due to the ERB extension

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,7 +1,12 @@
 //= link_tree ../images
 //= link_tree ../builds
+
+/* Handle these JS files directly with Sprockets */
 //= link application.js
 //= link bp_annotator.js
 //= link bp_annotatorplus.js
-//= link jquery-ui.css
 //= link vendor.js
+
+/* Handle these CSS files directly with Sprockets */
+//= link jquery-ui.css
+//= link theme-variables.scss
