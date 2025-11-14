2627bc35c9c253b57830498a090261365695565c
Jennifer Vendetti <vendetti@stanford.edu>
2025-11-14 12:33:17 -0800

    Resolve some RuboCop warnings

--- app/components/tree_infinite_scroll_component.rb
+++ app/components/tree_infinite_scroll_component.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
+# Component for rendering paginated tree data with infinite scroll
 class TreeInfiniteScrollComponent < ViewComponent::Base
-
   attr_reader :collection
+
   renders_one :error
 
+  # rubocop:disable Metrics/ParameterLists
   def initialize(id:, collection:, next_url:, current_page:, next_page:, auto_click: false)
     super
     @id = id
@@ -14,6 +16,7 @@ class TreeInfiniteScrollComponent < ViewComponent::Base
     @next_page = next_page
     @auto_click = auto_click
   end
+  # rubocop:enable Metrics/ParameterLists
 
   def auto_click?
     @auto_click
