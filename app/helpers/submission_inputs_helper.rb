445a22e80690ea835fa762227c68b33407cb57ea
Jennifer Vendetti <vendetti@stanford.edu>
2025-11-20 16:50:56 -0800

    Fix old-style slot setter

--- app/helpers/submission_inputs_helper.rb
+++ app/helpers/submission_inputs_helper.rb
@@ -172,7 +172,7 @@ module SubmissionInputsHelper
 
   def has_ontology_language_input(submission = @submission)
     render(Layout::RevealComponent.new(possible_values: %w[SKOS OBO UMLS OWL], selected: submission.hasOntologyLanguage)) do |c|
-      c.button do
+      c.with_button do
         attribute_input("hasOntologyLanguage")
       end
 
