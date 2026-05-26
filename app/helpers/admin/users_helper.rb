d4a3f08e016209f4dfaad71f7910335b9111e26c
Jennifer Vendetti <vendetti@stanford.edu>
2026-05-26 15:31:44 -0700

    Fix some RuboCop warnings

--- app/helpers/admin/users_helper.rb
+++ app/helpers/admin/users_helper.rb
@@ -1,27 +1,32 @@
-module Admin::UsersHelper
+# frozen_string_literal: true
 
-  def user_datatable_row(user)
-    username = user['username'].to_s
-    user_id = user['@id'].to_s
+module Admin
+  module UsersHelper
+    # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+    def user_datatable_row(user)
+      username = user['username'].to_s
+      user_id = user['@id'].to_s
 
-    [
-      h(user['firstName']),
-      h(user['lastName']),
-      h(username),
-      h(user['email']),
-      h(Array(user['role']).join(', ')),
-      link_to(user_id, user_id),
-      h(user['created']),
-      user_datatable_actions(username)
-    ]
+      [
+        h(user['firstName']),
+        h(user['lastName']),
+        h(username),
+        h(user['email']),
+        h(Array(user['role']).join(', ')),
+        link_to(user_id, user_id),
+        h(user['created']),
+        user_datatable_actions(username)
+      ]
+    end
+    # rubocop:enable Metrics/AbcSize, Metrics/MethodLength
+
+    def user_datatable_actions(username)
+      links = [
+        link_to('Detail', "/accounts/#{username}", class: 'mx-1'),
+        link_to('Delete', 'javascript:;', class: 'delete-user mx-1', data: { account_name: username }),
+        link_to('Login as', login_as_path(username: username), class: 'mx-1')
+      ]
+      safe_join(links, '|')
+    end
   end
-
-  def user_datatable_actions(username)
-    safe_join([
-      link_to('Detail', "/accounts/#{username}", class: 'mx-1'),
-      link_to('Delete', 'javascript:;', class: 'delete-user mx-1', data: { account_name: username }),
-      link_to('Login as', login_as_path(username: username), class: 'mx-1')
-    ], '|')
-  end
-
 end
